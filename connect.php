077190391bb625a7b2cd100493837c2bd85e412c
otkz44 <otomine.k@gmail.com>
2022-01-06 22:41:11 +0900

    add: todolist作成
    first commit

---
+++ connect.php
@@ -0,0 +1,36 @@
+<?php
+//入力内容を変数に格納する
+$text = $_POST['text'];
+$data_id = $_POST['data_id'];
+
+//接続情報を設定
+$dsn = 'mysql:dbname=todolist;host=localhost;charset=utf8';
+$user = 'test';
+$password = 'test';
+
+var_dump($_POST);
+
+try {
+    //DB接続
+    $connect = new PDO($dsn, $user,$password);
+    
+    if($data_id === "") {
+        //入力データを登録
+        $query = "insert into todolist(message) values('$text');";
+        $stmt = $connect->prepare($query);
+        $stmt->execute();
+    } else {
+        //リストを削除する
+        $query = "delete from todolist where id = :id;";
+        $stmt = $connect->prepare($query);
+        $stmt->bindValue(':id', $data_id);
+        $stmt->execute();
+    }
+} catch (PDOException $e) {
+    echo 'DB接続エラー:'.$e->getMessage();
+}
+
+//リダイレクト
+header("Location: index.php");
+exit;
+?>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+//接続情報を設定
+$dsn = 'mysql:dbname=todolist;host=localhost;charset=utf8';
+$user = 'test';
+$password = 'test';
+
+try {
+    //DB接続
+    $connect = new PDO($dsn, $user,$password);
+    //todolistテーブルからデータを取得する
+    $query = "select * from todolist;";
+    $stmt = $connect->query($query);
+} catch (PDOException $e) {
+    echo 'エラー:'.$e->getMessage();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <title>ToDo List</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    </head>
+    <body>
+        <div id="wrap">
+            <h1>ToDo List</h1>
+            <form class="todo_list" action="connect.php" method="POST">
+                <div class="main">
+                    <div class="input">
+                        <input type="hidden" name="data_id" value="">
+                        <input type="text" name="text" class="input_text" placeholder="ここに入力してください。">
+                        <button type="submit" class="add_btn" name="data_id" value="">新規追加</button>
+                    </div>
+                    <div class="output">
+                        <ul class="list">
+                            <?php foreach ($stmt as $row) { ?> 
+                                <!-- DBから取得したデータをセットする -->
+                                <li>
+                                    <?= htmlspecialchars($row["message"], ENT_QUOTES)?>
+                                    <button type="submit" class="delete_btn" name="data_id" value="<?= $row['id'] ?>">削除</button>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <script src="script.js"></script>
+    </body>
+</html>
